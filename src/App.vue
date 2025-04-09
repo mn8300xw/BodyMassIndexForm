8082e2b5c3dfb2a3b72c964c8693c5cd200e54f9
Vanessa M. <mn8300xw@go.minnstate.edu>
2025-04-09 16:26:40 -0500

    vue push

---
+++ src/App.vue
@@ -0,0 +1,43 @@
+<script setup>
+import BodyMassIndexForm from './components/BodyMassIndexForm.vue'
+import { ref } from 'vue'
+
+ const height = ref("Height in meters")
+ const weight = ref("Weight in kilograms")
+
+const calculate = ref(0)
+
+function calculateBmi(height, weight) {
+   calculate.value = weight.value / ( height.value * height.value )
+  console.log(calculate.value)
+}
+
+</script>
+
+<template>
+  <h1>Body Mass Index</h1>
+
+  <BodyMassIndexForm
+
+      v-on:stats-entered="calculateBmi">
+
+  </BodyMassIndexForm>
+
+    {{ calculate }}
+
+</template>
+
+<style scoped>
+.logo {
+  height: 6em;
+  padding: 1.5em;
+  will-change: filter;
+  transition: filter 300ms;
+}
+.logo:hover {
+  filter: drop-shadow(0 0 2em #646cffaa);
+}
+.logo.vue:hover {
+  filter: drop-shadow(0 0 2em #42b883aa);
+}
+</style>
---
+++ src/components/BodyMassIndexForm.vue
@@ -0,0 +1,45 @@
+<script setup>
+import { ref } from 'vue'
+
+defineProps({
+
+})
+
+const height = ref('')
+const weight = ref('')
+
+const emit = defineEmits( [
+  'stats-entered'
+])
+
+
+
+function choiceMade(height, weight) {
+  emit('stats-entered', height.value, weight.value)
+}
+
+
+</script>
+
+<template>
+  <h2>Enter your height and weight</h2>
+  <div>
+    <label for="height-1">Height in Meters: </label>
+
+    <input type="number" id="height-1" v-model.number="height" >
+
+    <br>
+    <label for="weight-2">Weight in Kilograms</label>
+
+    <input type="number" id="weight-2" v-model.number="weight" >
+
+    <br>
+    <label></label>
+    <button type="submit" v-on:click="choiceMade">Calculate</button>
+  </div>
+
+</template>
+
+<style scoped>
+
+</style>
